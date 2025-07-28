9c59bf33fd3b928cbba5048aca06f04131c2831e
sahil-1729 <sahil.maji@gmail.com>
2025-07-28 22:25:58 +0530

    second commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,11 @@
-import Image from "next/image";
+import Dashboard from "@/components/dashboard";
+import Graphs from "@/components/graphs";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex gap-4 flex-row">
+      <Dashboard />
+      <Graphs />
     </div>
   );
 }
